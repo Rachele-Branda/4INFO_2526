90ca3300ddd45b467f4f07aebf2db0a10c9c6d35
Branda-Rachele <rachele.branda@istitutoagnellli.it>
2026-01-22 15:37:29 +0100

    consegna es23

--- TPSI/branda_rachele/esercizi_tdd_STUDENTI/esercizio_js_23/soluzione.js
+++ TPSI/branda_rachele/esercizi_tdd_STUDENTI/esercizio_js_23/soluzione.js
@@ -1,4 +1,5 @@
 export const abbreviazione = (str) => {
-    //INSERISCI QUI IL TUO CODICE
+    str = str.split(" ")
+    return `${str[0].toUpperCase().charAt(0)}${str[0].slice(1)} ${str[1].toUpperCase().charAt(0)}.`
 }
 
