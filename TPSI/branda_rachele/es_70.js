6cf8a5737c18ef74835b396d343036ce620c020d
Branda-Rachele <rachele.branda@istitutoagnelli.it>
2026-04-23 00:25:39 +0200

    es70

---
+++ TPSI/branda_rachele/es_70.js
@@ -0,0 +1,45 @@
+class Calcolatrice{
+    constructor(str){
+        this.stringa = str
+    }
+
+    converti(){
+        let currentOp = "+"
+        let espressione  = []
+        this.stringa.split(" ").forEach(el => {
+        if (!isNaN(el) && el !== " "){
+            espressione.push({
+                val: Number(el),
+                op: currentOp
+            });
+        }
+        else if (["+", "-", "*", "/"].includes(el)){
+            currentOp = el}
+        })
+        return this.calcola(espressione)
+    }
+
+    calcola(espressione){
+    const ris = espressione.reduce((acc, current) => {
+        if (current.op === '+') {
+            return acc + current.val;
+        } else if (current.op === '-') {
+         return acc - current.val;
+        } else if (current.op === '*') {
+            return acc * current.val;
+        } else if (current.op === '/') {
+            return acc / current.val;
+        }   
+        return acc;
+        }, 0)
+        return ris
+    }
+}
+
+let Calc = new Calcolatrice("6 - 2 * 5 + 8 / 4")
+console.log(calc.converti())
+let calc = new Calcolatrice("11 * 3 + 5 - 5 / 11")
+console.log(calc.converti())
+calc = new Calcolatrice("29 + 2 - 7 / 3 / 2 + 1 * 2")
+console.log(calc.converti())
+
